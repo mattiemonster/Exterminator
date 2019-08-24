c200c8bd72560dc379ff5389dffe371cf689e104
Mattie <mattbware1@gmail.com>
2019-08-24 21:25:43 +0100

    Basic mouse look

--- Assets/Scripts/PlayerCamera.cs
+++ Assets/Scripts/PlayerCamera.cs
@@ -9,16 +9,47 @@ public class PlayerCamera : MonoBehaviour
     public float heightOffset = 0.5f;
     public float lookSpeed = 3;
     Vector2 rotation = new Vector2(0, 0);
+    bool mouseLocked = true;
+
+    void Start()
+    {
+        MouseLock(true);
+    }
+
+    public void MouseLock(bool value)
+    {
+        switch (value)
+        {
+            case true:
+                Cursor.lockState = CursorLockMode.Locked;
+                Cursor.visible = false;
+                break;
+            case false:
+                Cursor.lockState = CursorLockMode.None;
+                Cursor.visible = true;
+                break;
+        }
+        mouseLocked = value;
+    }
 
     void Update()
     {
-        Vector3 newPos = playerObject.transform.position;
-        newPos.y += heightOffset;
-        transform.position = newPos;
+        if (mouseLocked)
+        {
+            Vector3 newPos = playerObject.transform.position;
+            newPos.y += heightOffset;
+            transform.position = newPos;
 
-        rotation.y += Input.GetAxis("Mouse X");
-        rotation.x += -Input.GetAxis("Mouse Y");
-        rotation.x = Mathf.Clamp(rotation.x, -20f, 20f);
-        transform.eulerAngles = (Vector2)rotation * lookSpeed;
+            rotation.y += Input.GetAxis("Mouse X");
+            rotation.x += -Input.GetAxis("Mouse Y");
+            rotation.x = Mathf.Clamp(rotation.x, -20f, 20f);
+            transform.eulerAngles = (Vector2)rotation * lookSpeed;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+            MouseLock(!mouseLocked);
+
+        if (Input.GetMouseButtonDown(0) && !mouseLocked)
+            MouseLock(true);
     }
 }
