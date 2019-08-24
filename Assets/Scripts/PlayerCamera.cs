79ddebcfadb6794dadebc71aecc2a0e455e91496
Mattie <mattbware1@gmail.com>
2019-08-24 21:17:42 +0100

    Mouse look

--- Assets/Scripts/PlayerCamera.cs
+++ Assets/Scripts/PlayerCamera.cs
@@ -6,12 +6,19 @@ public class PlayerCamera : MonoBehaviour
     public GameObject playerObject;
 
     [Header("Values")]
-    public float heightOffset;
+    public float heightOffset = 0.5f;
+    public float lookSpeed = 3;
+    Vector2 rotation = new Vector2(0, 0);
 
     void Update()
     {
         Vector3 newPos = playerObject.transform.position;
         newPos.y += heightOffset;
         transform.position = newPos;
+
+        rotation.y += Input.GetAxis("Mouse X");
+        rotation.x += -Input.GetAxis("Mouse Y");
+        rotation.x = Mathf.Clamp(rotation.x, -20f, 20f);
+        transform.eulerAngles = (Vector2)rotation * lookSpeed;
     }
 }
