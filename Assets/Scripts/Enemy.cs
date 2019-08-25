61b6facd6a7d29d98c8db21645ab694bce4e6f36
Mattie <mattbware1@gmail.com>
2019-08-25 14:36:09 +0100

    Enemy asset, tooltips and moved scripts

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Enemy", menuName = "Game/Enemy")]
+public class Enemy : ScriptableObject
+{
+    [Tooltip("Enemy name")]
+    public string enemyName = "Enemy";
+    [Tooltip("Max and starting health of the enemy")]
+    public float maxHealth = 10f;
+    [Tooltip("Damage enemy does to player with its melee or ranged attack")]
+    public float damage = 5f;
+    [Tooltip("Speed at which enemy moves to player")]
+    public float movementSpeed = 12f;
+}
--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -9,13 +9,20 @@ public enum AmmoType
 [CreateAssetMenu(fileName = "New Weapon", menuName = "Game/Weapon")]
 public class Weapon : ScriptableObject
 {
+    [Tooltip("Weapon name shown in the current weapon UI.")]
     public string weaponName = "New Weapon";
     public int maxReserveAmmo = 150;
     public int maxCurrentAmmo = 20;
+    [Tooltip("Damage weapon does to enemies and/or player if enemy carries it.")]
     public float damage = 5;
+    [Tooltip("How fast the weapon can fire. Smaller is faster.")]
     public float fireSpeed = 2f;
+    [Tooltip("How far the hitscan ray can travel.")]
     public float range = 100f;
+    [Tooltip("Type of ammo for the weapon to fire. Hitscan shoots an invisble ray and damages hit enemies, projectile fires a damaging projectile based of a prefab.")]
     public AmmoType ammoType = AmmoType.Hitscan;
+    [Tooltip("Weapon prefab used for viewmodels.")]
     public GameObject weaponPrefab;
+    [Tooltip("Sprite shown in current weapon UI.")]
     public Sprite weaponIcon;
 }
