2aea5f50a68222593d5cfaffa5a97344b9f8b968
Mattie <mattbware1@gmail.com>
2019-08-27 22:55:09 +0100

    Movement tutorial, fixed nav mesh issue on stairs

---
+++ Assets/MovementTutorial.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using TMPro;
+
+public class MovementTutorial : MonoBehaviour
+{
+    [Header("Scene References")]
+    public TextMeshProUGUI wUI;
+    public TextMeshProUGUI aUI, sUI, dUI;
+
+    [Header("Values")]
+    public Color pressedColour = Color.green;
+
+    private int progress = 0;
+    private bool w, a, s, d;
+
+    void Start()
+    {
+        GetComponent<Animator>().Play("OpenWASD");
+    }
+
+    void Update()
+    {
+        if (!w && Input.GetKeyDown(KeyCode.W))
+        {
+            wUI.color = pressedColour;
+            progress++;
+            w = true;
+        }
+
+        if (!a && Input.GetKeyDown(KeyCode.A))
+        {
+            aUI.color = pressedColour;
+            progress++;
+            a = true;
+        }
+
+        if (!s && Input.GetKeyDown(KeyCode.S))
+        {
+            sUI.color = pressedColour;
+            progress++;
+            s = true;
+        }
+
+        if (!d && Input.GetKeyDown(KeyCode.D))
+        {
+            dUI.color = pressedColour;
+            progress++;
+            d = true;
+        }
+
+        if (progress == 4)
+        {
+            GetComponent<Animator>().Play("CloseWASD");
+            Destroy(gameObject, 0.8f);
+        }
+    }
+}
