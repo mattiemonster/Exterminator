3b89fed3a649e0abd5ba8b7b6e934d2c1e78d077
Mattie <mattbware1@gmail.com>
2019-08-28 02:15:03 +0100

    Health pickup sound

--- Assets/Scripts/HealthPickup.cs
+++ Assets/Scripts/HealthPickup.cs
@@ -1,12 +1,23 @@
 ﻿using UnityEngine;
 
+[RequireComponent(typeof(AudioSource))]
 public class HealthPickup : MonoBehaviour
 {
     [Header("Scene References")]
     public Player player;
 
+    [Header("Asset References")]
+    public AudioClip healSound;
+
     [Header("Values")]
     public float healAmount = 15f;
+
+    private AudioSource audioSrc;
+
+    void Start()
+    {
+        audioSrc = GetComponent<AudioSource>();
+    }
 
     void OnTriggerEnter(Collider collider)
     {
@@ -17,8 +28,12 @@ public class HealthPickup : MonoBehaviour
             // Give player health
             player.Heal(healAmount);
 
+            // Play sound
+            audioSrc.clip = healSound;
+            audioSrc.Play();
+
             // Destroy object
-            Destroy(gameObject);
+            Destroy(gameObject, healSound.length + 0.05f);
         }
     }
 }
