3da0cdd6f2e2e85ca13d139c905381866ddd2915
Mattie <mattbware1@gmail.com>
2019-08-30 21:33:05 +0100

    Intro functionality

---
+++ Assets/Scripts/IntroManager.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class IntroManager : MonoBehaviour
+{
+    public TextTyper textTyper;
+    public GameObject textObject;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            textTyper.LoadNextString();
+            textObject.GetComponent<Animator>().StopPlayback();
+            textObject.GetComponent<Animator>().Play("ContinueAnim");
+        }
+    }
+}
---
+++ Assets/Scripts/TextTyper.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class TextTyper : MonoBehaviour
+{
+    public List<string> entries;
+    public TextMeshProUGUI text;
+
+    private int stringIndex = 0;
+    private int charIndex;
+    private int maxChar;
+
+    void Start()
+    {
+        text.text = "";
+        if (entries.Count == 0) Debug.LogError("No text items");
+        LoadString(0);
+        InvokeRepeating("Type", 0f, 0.05f);
+    }
+
+    void Type()
+    {
+        if (charIndex == maxChar) return;
+        text.text = text.text + entries[stringIndex][charIndex];
+        charIndex++;
+    }
+
+    void LoadString(int index)
+    {
+        text.text = "";
+        maxChar = entries[stringIndex].Length;
+        charIndex = 0;
+    }
+
+    public void LoadNextString()
+    {
+        stringIndex++;
+        LoadString(stringIndex);
+    }
+}
